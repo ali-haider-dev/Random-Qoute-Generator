13d1bc522568035356cd00aebc74bd22e06c7355
ali-haider-dev <75415715+ali-haider-dev@users.noreply.github.com>
2024-07-17 15:06:45 +0500

    Completed

--- src/App.jsx
+++ src/App.jsx
@@ -2,22 +2,22 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import "./App.css";
 import { faQuoteLeftAlt } from "@fortawesome/free-solid-svg-icons";
 import { faQuoteRightAlt } from "@fortawesome/free-solid-svg-icons";
-import { faCopy } from "@fortawesome/free-solid-svg-icons";
 
 import { useState, useEffect } from "react";
 import Swal from "sweetalert2";
 function App() {
-  const [quote, setQoute] = useState({});
+  const [quotes, setQoutes] = useState({});
   const [fetching, setFetching] = useState(false);
   const fetchQuotes = async () => {
+    
     try {
       setFetching(true);
       const response = await fetch("https://type.fit/api/quotes");
       const data = await response.json();
-      const randomQuote = data[Math.floor(Math.random() * data.length)];
-      setQoute(randomQuote);
+      const randomData = data[Math.floor(Math.random() * data.length)];
+      setQoutes(randomData);
       setFetching(false);
-      console.log(randomQuote);
+      console.log(randomData);
     } catch (error) {
       Swal.fire({
         icon: "error",
@@ -28,24 +28,31 @@ function App() {
     }
   };
 
+  const Speech =( quoteText,authorName)=>{
+    console.log(`${quoteText} by ${authorName}`)
+    let synth = window.speechSynthesis
+    let utterance = new SpeechSynthesisUtterance(`${quoteText} by ${authorName}`);
+    synth.speak(utterance);
+  }
+
   useEffect(() => {
     fetchQuotes();
   }, []);
 
-  console.log(quote);
+  console.log(quotes);
 
   return (
     <div className="main">
       <div className="container">
         <h1 className="title">Qoute Of the day</h1>
-        {quote?.text ? (
+        {quotes?.text  ? (
           <>
             <p className="qoute">
               <FontAwesomeIcon
                 icon={faQuoteLeftAlt}
                 style={{ marginRight: "5px" }}
               />
-              {quote.text} <FontAwesomeIcon icon={faQuoteRightAlt} />
+              {quotes.text} <FontAwesomeIcon icon={faQuoteRightAlt} />
             </p>
             <div className="author">
               <p
@@ -56,24 +63,21 @@ function App() {
                   marginRight: "3px",
                 }}
               ></p>
-              {quote.author.split(",")[0]}
+              {quotes.author.split(",")[0]}
             </div>
             <hr />
             <div className="Buttons">
               <div className="options">
                 <i
                   className="fas fa-volume-up option-button"
-                  onClick={() => {}}
+                  onClick={() => Speech(quotes.text,quotes.author.split(",")[0])}
                 />
-                <i
-                  className="fa-brands fa-twitter option-button"
-                  onClick={() => {}}
-                />
-                <i className="fas fa-copy option-button" onClick={() => {}} />
+                
+                <i className="fas fa-copy option-button" onClick={() => { navigator.clipboard.writeText(quotes.text + "\n " + quotes.author.split(",")[0]); Swal.fire({icon:"success",title:"Qoute is succesfully copied to Clipboard",timer:1500})}} />
               </div>
               <button
                 className="qoute-button"
-                disabled={fetching ? true : false}
+                // disabled={fetching ? true : false}
                 onClick={() => fetchQuotes()}
               >
                 {" "}
@@ -82,7 +86,9 @@ function App() {
             </div>
           </>
         ) : (
+          
           <>
+       
             <p className="qoute">
               <FontAwesomeIcon
                 icon={faQuoteLeftAlt}
@@ -106,45 +112,38 @@ function App() {
             <div className="Buttons">
               <div className="options">
                 <i
-                  className="fas fa-volume-up"
+                  className="fas fa-volume-up option-button"
                   style={{
                     border: "1px solid blue",
                     borderRadius: "50%",
                     padding: "5px",
                   }}
-                  onClick={() => {}}
+                  onClick={() => {Speech("Never giveup because you never know if the next try is going to be the one that works.","mary Kay Ash")}}
                 />
+               
                 <i
-                  className="fa-brands fa-twitter"
+                  className="fas fa-copy option-button"
                   style={{
                     border: "1px solid blue",
                     borderRadius: "50%",
                     padding: "5px",
                     marginLeft: "5px",
                   }}
-                  onClick={() => {}}
-                />
-                <i
-                  className="fas fa-copy"
-                  style={{
-                    border: "1px solid blue",
-                    borderRadius: "50%",
-                    padding: "5px",
-                    marginLeft: "5px",
-                  }}
-                  onClick={() => {}}
+                  onClick={() => {navigator.clipboard.writeText(" Never giveup because you never know if the next try is going to bethe one that works."+ "\n " + "Mary Kay Ash"); Swal.fire({icon:"success",title:"Qoute is succesfully copied to Clipboard",timer:1500})}}
                 />
               </div>
               <button
                 className="qoute-button"
-                disabled={fetching ? "true" : "false"}
+              
+                onClick={()=>fetchQuotes()}
               >
-                {" "}
+               
                 {"New Qoute"}
               </button>
             </div>
           </>
-        )}
+        )
+        }
       </div>
     </div>
   );
